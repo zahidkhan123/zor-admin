a99192e3e014c4fb84daff5923e1cb8cf1637164
Zahid khan <zahidkhandev@gmail.com>
2025-06-30 18:42:44 +0500

    origin changed

--- src/services/api.js
+++ src/services/api.js
@@ -6,12 +6,13 @@ const getToken = () => {
   const localToken = localStorage.getItem('token')
   return localToken
 }
+
 // baseURL: 'https://zor-development.onrender.com/api/v1',
 // baseURL: 'http://localhost:3000/api/v1',
 
 // Create axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://zor-development.onrender.com/api/v1',
   headers: {
     Authorization: `Bearer ${getToken()}`,
   },
